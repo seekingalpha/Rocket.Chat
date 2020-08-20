10af010b75aa048b6cd0f7da59dff5ffa6b06a82
Noach Magedman <noach@seekingalpha.com>
2020-08-20 12:58:59 +0300

    PATCH: Disable Reply-To in Offline Email notifications
    RC v3.4.0 adds a Reply-To header containing the user’s email address.
    Previously, there was no Reply-To header at all.  The From header was
    (and still is) the commenter's real name and a site-wide configured
    email address, “account@seekingalpha.com”.
    
    The new feature was intended to **prevent** a privacy issue – if a user
    would reply to an email thinking it would go to the commenter, he might
    send a confidential message to the account@seekingalpha.com mailbox.
    So with the Reply-To, email would go to the intended recipient.
    
    However, AFAICT, the user's email address is not displayed to any other
    (non-admin) users anywhere within the RC UI, so we've always considered
    the email address to be a private field.  However, the Reply-To exposes
    this private email address.
    
    Solution:
    Remove the Reply-To header.
    
    In the future, we may also change the From/Reply-To address to "no-reply@"
    
    https://seekingalpha.atlassian.net/browse/MP-931

--- app/lib/server/functions/notifications/email.js
+++ app/lib/server/functions/notifications/email.js
@@ -117,7 +117,6 @@ function generateNameEmail(name, email) {
 export function getEmailData({
 	message,
 	receiver,
-	sender,
 	subscription,
 	room,
 	emailAddress,
@@ -158,11 +157,6 @@ export function getEmailData({
 		},
 		headers: {},
 	};
-
-	if (sender.emails?.length > 0) {
-		const [senderEmail] = sender.emails;
-		email.headers['Reply-To'] = generateNameEmail(username, senderEmail.address);
-	}
 
 	// If direct reply enabled, email content with headers
 	if (settings.get('Direct_Reply_Enable')) {
