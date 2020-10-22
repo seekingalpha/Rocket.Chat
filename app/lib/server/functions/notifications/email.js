1e1f26f0f54b470e4eb08baa874a193b4f22207e
Daria Kondrashova <daria@seekingalpha.com>
2020-10-22 13:41:09 +0300

    PATCH: [MP-1010] Do not HTML-escape email subject lines
    Upstream started escaping all template placeholder-values
    in replacekey(), making the false assumptions that
    (1) they ought to be escaped
    (2) they are not yet escaped
    See commit 362366b25aa4298b35df7a6540b9362225f9054a
    
    As Noach pointed out to Rodrigo in a DM on open.RC,
    many callers already escape their values. e.g.
    app/authentication/server/startup/index.js
    See commit 0aada1571ea8b6ca6bf75f93fd969b82a982d3ca
    
    And in the case of email subject lines, strings ought
    not to be escaped; subject lines are plaintext, not HTML.
    
    Ideally, we should change how the template expansion works:
      Have a helper method for HTML-escaping all values.
      Let the caller invoke it before passing the result into
      replace/replacekey.
    
    But that's really for upstream to figure out.  I don't want
    to invest all the time for confirming we have no regressions.
    This patch gets the job done for now.  The caller specifies
    which KV-pairs should _not_ be escaped during expansion.
    The approach is modeled after upstream’s replaceEscaped()
    (which incidentally, looks like it double-encodes!)
    
    -- (commit message by Noach)

--- app/lib/server/functions/notifications/email.js
+++ app/lib/server/functions/notifications/email.js
@@ -136,7 +136,7 @@ export function getEmailData({
 	const emailSubject = Mailer.replace(settings.get(subjectKey), {
 		user: username,
 		room: roomTypes.getRoomName(room.t, room),
-	});
+	}, ['room']);
 	const content = getEmailContent({
 		message,
 		user: receiver,
