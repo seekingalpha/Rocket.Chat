9d1b7299fc01cf7e53caad206235ad7220f14c95
Noach Magedman <noach@seekingalpha.com>
2023-02-27 11:17:45 +0200

    PATCH: API users.setAvatar: Return the new avatarETag
    NOTE: This disables much of the TypeScript checking.
    TODO: Update the type-specs properly.
    
    Note that upstream also moved the `if (updater)/else` block
    out of the setTimeout block, however they were more cautious than I:
    I ran the `else await Users.setAvatarData` immediately, whereas they
    retained the setTimeout for that one codepath:
    
    Diff:
        `-` was my implementation.
        `+` is their re-implementation.
    ```
     if (service) {
             if (updater) {
                     updater.set('avatarOrigin', origin);
                     updater.set('avatarETag', avatarETag);
             } else {
    -                await Users.setAvatarData(user._id, service, avatarETag);
    +                // TODO: Why was this timeout added?
    +                setTimeout(async () => Users.setAvatarData(user._id, service, avatarETag, { session }), 500);
             }
    ```
    
    n.b. When they ask "Why was this timeout added?", they are not asking
    why it was added just now.  They are asking why it was added long ago
    in the old one-big-setTimeout implementation.

--- apps/meteor/app/api/server/v1/users.ts
+++ apps/meteor/app/api/server/v1/users.ts
@@ -246,6 +246,7 @@ API.v1.addRoute(
 	},
 );
 
+// @ DISABLED FOR NOW ts-ignore
 API.v1.addRoute(
 	'users.setAvatar',
 	{ authRequired: true, validateParams: isUsersSetAvatarProps },
@@ -273,8 +274,8 @@ API.v1.addRoute(
 			}
 
 			if (this.bodyParams.avatarUrl) {
-				await setUserAvatar(user, this.bodyParams.avatarUrl, '', 'url');
-				return API.v1.success();
+				const avatarETag = await setUserAvatar(user, this.bodyParams.avatarUrl, '', 'url');
+				return API.v1.success({ avatarETag });
 			}
 
 			const image = await getUploadFormData(
@@ -310,9 +311,9 @@ API.v1.addRoute(
 				}
 			}
 
-			await setUserAvatar(user, fileBuffer, mimetype, 'rest');
+			const avatarETag = await setUserAvatar(user, fileBuffer, mimetype, 'rest');
 
-			return API.v1.success();
+			return API.v1.success({ avatarETag });
 		},
 	},
 );
