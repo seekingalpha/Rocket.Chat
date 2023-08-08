8195407f4523f1972dc748d879495d0eae1fb8f7
Noach Magedman <noach@seekingalpha.com>
2023-08-09 00:46:17 +0300

    PATCH: Add API `users.unreadCount`
    A wrapper around `Subscriptions.getBadgeCount`, so that
    SAPI can show the Unread Message Count in the navbar.
    
    WARNING: No security controls have been implemented on this.
             Anyone can request the unread count of any other user!

--- apps/meteor/app/api/server/v1/users.ts
+++ apps/meteor/app/api/server/v1/users.ts
@@ -56,6 +56,23 @@ import { isValidQuery } from '../lib/isValidQuery';
 import { findPaginatedUsersByStatus, findUsersToAutocomplete, getInclusiveFields, getNonEmptyFields, getNonEmptyQuery } from '../lib/users';
 
 API.v1.addRoute(
+	'users.unreadCount',
+	{ authRequired: true },
+	{
+		async get() {
+			const user = await getUserFromParams(this.queryParams);
+			const unreadCount = (await Subscriptions.getBadgeCount(user._id)) || 0;
+
+			return API.v1.success({
+				data: {
+					unread: unreadCount,
+				},
+			});
+		},
+	},
+);
+
+API.v1.addRoute(
 	'users.getAvatar',
 	{ authRequired: false },
 	{
