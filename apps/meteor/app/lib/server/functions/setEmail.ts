b18fca5ef6ee3a6788185ca053e4868dcb7e4945
Noach Magedman <noach@seekingalpha.com>
2023-08-09 10:48:52 +0300

    PATCH: Don't send verification emails by default
    When configuring a user's email address, we can optionally
    send a verification email.  This now defaults to *not*
    sending.
    
    • TODO: SAPI's RCASynchronizer should pass shouldSendVerificationEmail=true explicitly, so that
      we don't need this patch anymore.
    
    • TODO: It used to be that setting an email address would set verified:false and we would then
      update it with verified:true.  setEmail now has a parameter to set verified at the time of
      setting the email (default: false).  SAPI's RCASynchronizer should merge these two calls into one.

--- apps/meteor/app/lib/server/functions/setEmail.ts
+++ apps/meteor/app/lib/server/functions/setEmail.ts
@@ -43,7 +43,7 @@ const _sendEmailChangeNotification = async function (to: string, newEmail: strin
 const _setEmail = async function (
 	userId: string,
 	email: string,
-	shouldSendVerificationEmail = true,
+	shouldSendVerificationEmail = false,
 	verified = false,
 	updater?: Updater<IUser>,
 ) {
