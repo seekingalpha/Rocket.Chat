c3cd09ae14d5cc2b1d2d17fd1e968f3f815cb2f4
Noach Magedman <noach@seekingalpha.com>
2023-08-24 18:37:58 +0300

    PATCH: Customize the Welcome message on the login screen
    In RC 6.x, upstream added a lengthy welcome message to the login page,
    hardcoded to:
    
    	> Welcome to {siteName} workspace
    
    where {siteName} is currently "Seeking Alpha Chat".
    
    I find the text of "workspace" to be awkward, but it's not configurable.
    
    There is an option to disable it entirely, but then it looks empty.
    There used to be a large SA logo, but they've shrunk the logo down considerably.
    
    Replace it with the text:
    
    	   Seeking Alpha
    	  Investing Groups
    
    Use HTML to explicitly break it at the right point.

--- packages/web-ui-registration/src/components/RegisterTitle.tsx
+++ packages/web-ui-registration/src/components/RegisterTitle.tsx
@@ -1,9 +1,7 @@
 import { useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
-import { Trans } from 'react-i18next';
 
 export const RegisterTitle = (): ReactElement | null => {
-	const siteName = useSetting<string>('Site_Name');
 	const hideTitle = useSetting<boolean>('Layout_Login_Hide_Title');
 
 	if (hideTitle) {
@@ -12,7 +10,9 @@ export const RegisterTitle = (): ReactElement | null => {
 
 	return (
 		<span id='welcomeTitle'>
-			<Trans i18nKey='registration.component.welcome'>Welcome to {siteName} workspace</Trans>
+			Seeking Alpha
+			<br />
+			Investing Groups
 		</span>
 	);
 };
