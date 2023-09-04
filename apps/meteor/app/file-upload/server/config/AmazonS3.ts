seekingalpha/Rocket.Chat

commit 90238b9c1538a1d64268926a1861def4ff1bfcc9

refactor: Convert our Upload Code to TS (#28711)

apps/meteor/app/file-upload/server/config/AmazonS3.ts
@@ -1,39 +1,36 @@
 import http from 'http';
 import https from 'https';
+import URL from 'url';
 
 import _ from 'underscore';
 
 import { settings } from '../../../settings/server';
 import { FileUploadClass, FileUpload } from '../lib/FileUpload';
-import '../../ufs/AmazonS3/server.js';
-import { SystemLogger } from '../../../../server/lib/logger/system';
+import type { S3Options } from '../../ufs/AmazonS3/server';
 
-const get = function (file, req, res) {
-	const forceDownload = typeof req.query.download !== 'undefined';
+const get: FileUploadClass['get'] = async function (this: FileUploadClass, file, req, res) {
+	const { query } = URL.parse(req.url || '', true);
+	const forceDownload = typeof query.download !== 'undefined';
 
-	this.store.getRedirectURL(file, forceDownload, (err, fileUrl) => {
-		if (err) {
-			return SystemLogger.error(err);
-		}
+	const fileUrl = await this.store.getRedirectURL(file, forceDownload);
+	if (!fileUrl || !file.store) {
+		res.end();
+		return;
+	}
 
-		if (!fileUrl) {
-			return res.end();
-		}
+	const storeType = file.store.split(':').pop();
+	if (settings.get(`FileUpload_S3_Proxy_${storeType}`)) {
+		const request = /^https:/.test(fileUrl) ? https : http;
 
-		const storeType = file.store.split(':').pop();
-		if (settings.get(`FileUpload_S3_Proxy_${storeType}`)) {
-			const request = /^https:/.test(fileUrl) ? https : http;
+		FileUpload.proxyFile(file.name || '', fileUrl, forceDownload, request, req, res);
+		return;
+	}
 
-			return FileUpload.proxyFile(file.name, fileUrl, forceDownload, request, req, res);
-		}
-
-		return FileUpload.redirectToFile(fileUrl, req, res);
-	});
+	FileUpload.redirectToFile(fileUrl, req, res);
 };
 
-const copy = function (file, out) {
-	const fileUrl = this.store.getRedirectURL(file);
-
+const copy: FileUploadClass['copy'] = async function (this: FileUploadClass, file, out) {
+	const fileUrl = await this.store.getRedirectURL(file);
 	if (fileUrl) {
 		const request = /^https:/.test(fileUrl) ? https : http;
 		request.get(fileUrl, (fileRes) => fileRes.pipe(out));
@@ -64,22 +61,22 @@ const AmazonS3UserDataFiles = new FileUploadClass({
 });
 
 const configure = _.debounce(function () {
-	const Bucket = settings.get('FileUpload_S3_Bucket');
-	const Acl = settings.get('FileUpload_S3_Acl');
-	const AWSAccessKeyId = settings.get('FileUpload_S3_AWSAccessKeyId');
-	const AWSSecretAccessKey = settings.get('FileUpload_S3_AWSSecretAccessKey');
-	const URLExpiryTimeSpan = settings.get('FileUpload_S3_URLExpiryTimeSpan');
-	const Region = settings.get('FileUpload_S3_Region');
-	const SignatureVersion = settings.get('FileUpload_S3_SignatureVersion');
-	const ForcePathStyle = settings.get('FileUpload_S3_ForcePathStyle');
+	const Bucket = settings.get<string>('FileUpload_S3_Bucket');
+	const Acl = settings.get<string>('FileUpload_S3_Acl');
+	const AWSAccessKeyId = settings.get<string>('FileUpload_S3_AWSAccessKeyId');
+	const AWSSecretAccessKey = settings.get<string>('FileUpload_S3_AWSSecretAccessKey');
+	const URLExpiryTimeSpan = settings.get<number>('FileUpload_S3_URLExpiryTimeSpan');
+	const Region = settings.get<string>('FileUpload_S3_Region');
+	const SignatureVersion = settings.get<string>('FileUpload_S3_SignatureVersion');
+	const ForcePathStyle = settings.get<boolean>('FileUpload_S3_ForcePathStyle');
 	// const CDN = RocketChat.settings.get('FileUpload_S3_CDN');
-	const BucketURL = settings.get('FileUpload_S3_BucketURL');
+	const BucketURL = settings.get<string>('FileUpload_S3_BucketURL');
 
 	if (!Bucket) {
 		return;
 	}
 
-	const config = {
+	const config: Omit<S3Options, 'name' | 'getPath'> = {
 		connection: {
 			signatureVersion: SignatureVersion,
 			s3ForcePathStyle: ForcePathStyle,

apps/meteor/app/apps/server/bridges/uploads.ts
@@ -32,9 +32,13 @@ export class AppUploadBridge extends UploadBridge {
 		const rocketChatUpload = this.orch.getConverters()?.get('uploads').convertToRocketChat(upload);
 
 		return new Promise((resolve, reject) => {
-			FileUpload.getBuffer(rocketChatUpload, (error: Error, result: Buffer) => {
+			FileUpload.getBuffer(rocketChatUpload, (error?: Error, result?: Buffer | false) => {
 				if (error) {
 					return reject(error);
+				}
+
+				if (!(result instanceof Buffer)) {
+					return reject(new Error('Unknown error'));
 				}
 
 				resolve(result);

apps/meteor/app/file-upload/client/lib/fileUploadHandler.ts
@@ -3,10 +3,12 @@ import { Accounts } from 'meteor/accounts-base';
 import { Tracker } from 'meteor/tracker';
 
 Tracker.autorun(function () {
-	if (Meteor.userId()) {
+	const userId = Meteor.userId();
+
+	if (userId) {
 		const secure = location.protocol === 'https:' ? '; secure' : '';
 
-		document.cookie = `rc_uid=${escape(Meteor.userId())}; path=/${secure}`;
-		document.cookie = `rc_token=${escape(Accounts._storedLoginToken())}; path=/${secure}`;
+		document.cookie = `rc_uid=${escape(userId)}; path=/${secure}`;
+		document.cookie = `rc_token=${escape(Accounts._storedLoginToken() as string)}; path=/${secure}`;
 	}
 });

apps/meteor/app/file-upload/lib/FileUploadBase.ts
@@ -5,11 +5,11 @@ import { Meteor } from 'meteor/meteor';
 import { UploadFS } from '../../../server/ufs';
 import { canAccessRoomAsync } from '../../authorization/server';
 import { hasPermissionAsync } from '../../authorization/server/functions/hasPermission';
-import { settings } from '../../settings';
+import { settings } from '../../settings/server';
 
 // set ufs temp dir to $TMPDIR/ufs instead of /tmp/ufs if the variable is set
 if ('TMPDIR' in process.env) {
-	UploadFS.config.tmpDir = path.join(process.env.TMPDIR, 'ufs');
+	UploadFS.config.tmpDir = path.join(process.env.TMPDIR || '', 'ufs');
 }
 
 UploadFS.config.defaultStorePermissions = new UploadFS.StorePermissions({
@@ -19,29 +19,39 @@ UploadFS.config.defaultStorePermissions = new UploadFS.StorePermissions({
 		}
 
 		// allow inserts from slackbridge (message_id = slack-timestamp-milli)
-		if (doc && doc.message_id && doc.message_id.indexOf('slack-') === 0) {
+		if (doc?.message_id?.indexOf('slack-') === 0) {
 			return true;
 		}
 
 		// allow inserts to the UserDataFiles store
-		if (doc && doc.store && doc.store.split(':').pop() === 'UserDataFiles') {
+		if (doc?.store?.split(':').pop() === 'UserDataFiles') {
 			return true;
 		}
 
-		if (Promise.await(canAccessRoomAsync(null, null, doc))) {
+		if (Promise.await(canAccessRoomAsync(undefined, undefined, doc))) {
 			return true;
 		}
 
 		return false;
 	},
 	update(userId, doc) {
+		const cUserId = Meteor.userId();
+		if (!cUserId) {
+			return false;
+		}
 		return Promise.await(
-			hasPermissionAsync(Meteor.userId(), 'delete-message', doc.rid) || (settings.get('Message_AllowDeleting') && userId === doc.userId),
+			void hasPermissionAsync(cUserId, 'delete-message', doc.rid) ||
+				(settings.get<boolean>('Message_AllowDeleting') && userId === doc.userId),
 		);
 	},
 	remove(userId, doc) {
+		const cUserId = Meteor.userId();
+		if (!cUserId) {
+			return false;
+		}
 		return Promise.await(
-			hasPermissionAsync(Meteor.userId(), 'delete-message', doc.rid) || (settings.get('Message_AllowDeleting') && userId === doc.userId),
+			void hasPermissionAsync(cUserId, 'delete-message', doc.rid) ||
+				(settings.get<boolean>('Message_AllowDeleting') && userId === doc.userId),
 		);
 	},
 });

apps/meteor/app/file-upload/server/config/FileSystem.ts
@@ -13,10 +13,13 @@ const FileSystemUploads = new FileUploadClass({
 	name: 'FileSystem:Uploads',
 	// store setted bellow
 
-	get(file, req, res) {
+	async get(file, req, res) {
+		if (!this.store || !file) {
+			return;
+		}
 		const filePath = this.store.getFilePath(file._id, file);
 
-		const options = {};
+		const options: { start?: number; end?: number } = {};
 
 		try {
 			const stat = statSync(filePath);
@@ -27,8 +30,8 @@ const FileSystemUploads = new FileUploadClass({
 			}
 
 			file = FileUpload.addExtensionTo(file);
-			res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name)}`);
-			res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
+			res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name || '')}`);
+			file.uploadedAt && res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
 			res.setHeader('Content-Type', file.type || 'application/octet-stream');
 
 			if (req.headers.range) {
@@ -46,7 +49,7 @@ const FileSystemUploads = new FileUploadClass({
 
 			// set content-length if range has not set
 			if (!res.getHeader('Content-Length')) {
-				res.setHeader('Content-Length', file.size);
+				res.setHeader('Content-Length', file.size || 0);
 			}
 
 			this.store.getReadStream(file._id, file, options).pipe(res);
@@ -56,12 +59,15 @@ const FileSystemUploads = new FileUploadClass({
 		}
 	},
 
-	copy(file, out) {
+	async copy(file, out) {
+		if (!this.store) {
+			return;
+		}
 		const filePath = this.store.getFilePath(file._id, file);
 		try {
 			const stat = statSync(filePath);
 
-			if (stat && stat.isFile()) {
+			if (stat?.isFile()) {
 				file = FileUpload.addExtensionTo(file);
 
 				this.store.getReadStream(file._id, file).pipe(out);
@@ -76,13 +82,16 @@ const FileSystemAvatars = new FileUploadClass({
 	name: 'FileSystem:Avatars',
 	// store setted bellow
 
-	get(file, req, res) {
+	async get(file, _req, res) {
+		if (!this.store) {
+			return;
+		}
 		const filePath = this.store.getFilePath(file._id, file);
 
 		try {
 			const stat = statSync(filePath);
 
-			if (stat && stat.isFile()) {
+			if (stat?.isFile()) {
 				file = FileUpload.addExtensionTo(file);
 
 				this.store.getReadStream(file._id, file).pipe(res);
@@ -97,18 +106,21 @@ const FileSystemAvatars = new FileUploadClass({
 const FileSystemUserDataFiles = new FileUploadClass({
 	name: 'FileSystem:UserDataFiles',
 
-	get(file, req, res) {
+	async get(file, _req, res) {
+		if (!this.store) {
+			return;
+		}
 		const filePath = this.store.getFilePath(file._id, file);
 
 		try {
 			const stat = statSync(filePath);
 
-			if (stat && stat.isFile()) {
+			if (stat?.isFile()) {
 				file = FileUpload.addExtensionTo(file);
-				res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name)}`);
-				res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
-				res.setHeader('Content-Type', file.type);
-				res.setHeader('Content-Length', file.size);
+				res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name || '')}`);
+				file.uploadedAt && res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
+				res.setHeader('Content-Type', file.type || '');
+				res.setHeader('Content-Length', file.size || 0);
 
 				this.store.getReadStream(file._id, file).pipe(res);
 			}
@@ -128,6 +140,6 @@ settings.watch('FileUpload_FileSystemPath', function () {
 	FileSystemAvatars.store = FileUpload.configureUploadsStore('Local', FileSystemAvatars.name, options);
 	FileSystemUserDataFiles.store = FileUpload.configureUploadsStore('Local', FileSystemUserDataFiles.name, options);
 
-	// DEPRECATED backwards compatibililty (remove)
+	// DEPRECATED backwards compatibility (remove)
 	UploadFS.getStores().fileSystem = UploadFS.getStores()[FileSystemUploads.name];
 });

apps/meteor/app/file-upload/server/config/GoogleStorage.ts
@@ -1,49 +1,43 @@
 import http from 'http';
 import https from 'https';
+import URL from 'url';
 
 import _ from 'underscore';
 
 import { FileUploadClass, FileUpload } from '../lib/FileUpload';
 import { settings } from '../../../settings/server';
-import '../../ufs/GoogleStorage/server.js';
-import { SystemLogger } from '../../../../server/lib/logger/system';
+import '../../ufs/GoogleStorage/server';
 
-const get = function (file, req, res) {
-	const forceDownload = typeof req.query.download !== 'undefined';
+const get: FileUploadClass['get'] = async function (this: FileUploadClass, file, req, res) {
+	const { query } = URL.parse(req.url || '', true);
+	const forceDownload = typeof query.download !== 'undefined';
 
-	this.store.getRedirectURL(file, forceDownload, (err, fileUrl) => {
-		if (err) {
-			return SystemLogger.error(err);
-		}
+	const fileUrl = await this.store.getRedirectURL(file, forceDownload);
+	if (!fileUrl || !file.store) {
+		res.end();
+		return;
+	}
 
-		if (!fileUrl) {
-			return res.end();
-		}
+	const storeType = file.store.split(':').pop();
+	if (settings.get(`FileUpload_GoogleStorage_Proxy_${storeType}`)) {
+		const request = /^https:/.test(fileUrl) ? https : http;
 
-		const storeType = file.store.split(':').pop();
-		if (settings.get(`FileUpload_GoogleStorage_Proxy_${storeType}`)) {
-			const request = /^https:/.test(fileUrl) ? https : http;
+		FileUpload.proxyFile(file.name || '', fileUrl, forceDownload, request, req, res);
+		return;
+	}
 
-			return FileUpload.proxyFile(file.name, fileUrl, forceDownload, request, req, res);
-		}
-
-		return FileUpload.redirectToFile(fileUrl, req, res);
-	});
+	FileUpload.redirectToFile(fileUrl, req, res);
 };
 
-const copy = function (file, out) {
-	this.store.getRedirectURL(file, false, (err, fileUrl) => {
-		if (err) {
-			SystemLogger.error(err);
-		}
+const copy: FileUploadClass['copy'] = async function (this: FileUploadClass, file, out) {
+	const fileUrl = await this.store.getRedirectURL(file, false);
 
-		if (fileUrl) {
-			const request = /^https:/.test(fileUrl) ? https : http;
-			request.get(fileUrl, (fileRes) => fileRes.pipe(out));
-		} else {
-			out.end();
-		}
-	});
+	if (fileUrl) {
+		const request = /^https:/.test(fileUrl) ? https : http;
+		request.get(fileUrl, (fileRes) => fileRes.pipe(out));
+	} else {
+		out.end();
+	}
 };
 
 const GoogleCloudStorageUploads = new FileUploadClass({

apps/meteor/app/file-upload/server/config/GridFS.ts
@@ -1,6 +1,9 @@
+import type { TransformCallback, TransformOptions } from 'stream';
 import stream from 'stream';
 import zlib from 'zlib';
-import util from 'util';
+import type * as http from 'http';
+
+import type { IUpload } from '@rocket.chat/core-typings';
 
 import { UploadFS } from '../../../../server/ufs';
 import { Logger } from '../../../logger/server';
@@ -9,48 +12,60 @@ import { getFileRange, setRangeHeaders } from '../lib/ranges';
 
 const logger = new Logger('FileUpload');
 
-function ExtractRange(options) {
-	if (!(this instanceof ExtractRange)) {
-		return new ExtractRange(options);
+class ExtractRange extends stream.Transform {
+	private start: number;
+
+	private stop: number;
+
+	private bytes_read: number;
+
+	constructor(options: TransformOptions & { start: number; stop: number }) {
+		super(options);
+
+		this.start = options.start;
+		this.stop = options.stop;
+		this.bytes_read = 0;
 	}
 
-	this.start = options.start;
-	this.stop = options.stop;
-	this.bytes_read = 0;
+	_transform(chunk: any, _enc: BufferEncoding, cb: TransformCallback) {
+		if (this.bytes_read > this.stop) {
+			// done reading
+			this.end();
+		} else if (this.bytes_read + chunk.length < this.start) {
+			// this chunk is still before the start byte
+		} else {
+			let start;
+			let stop;
 
-	stream.Transform.call(this, options);
+			if (this.start <= this.bytes_read) {
+				start = 0;
+			} else {
+				start = this.start - this.bytes_read;
+			}
+			if (this.stop - this.bytes_read + 1 < chunk.length) {
+				stop = this.stop - this.bytes_read + 1;
+			} else {
+				stop = chunk.length;
+			}
+			const newchunk = chunk.slice(start, stop);
+			this.push(newchunk);
+		}
+		this.bytes_read += chunk.length;
+		cb();
+	}
 }
-util.inherits(ExtractRange, stream.Transform);
-
-ExtractRange.prototype._transform = function (chunk, enc, cb) {
-	if (this.bytes_read > this.stop) {
-		// done reading
-		this.end();
-	} else if (this.bytes_read + chunk.length < this.start) {
-		// this chunk is still before the start byte
-	} else {
-		let start;
-		let stop;
-
-		if (this.start <= this.bytes_read) {
-			start = 0;
-		} else {
-			start = this.start - this.bytes_read;
-		}
-		if (this.stop - this.bytes_read + 1 < chunk.length) {
-			stop = this.stop - this.bytes_read + 1;
-		} else {
-			stop = chunk.length;
-		}
-		const newchunk = chunk.slice(start, stop);
-		this.push(newchunk);
-	}
-	this.bytes_read += chunk.length;
-	cb();
-};
 
 // code from: https://github.com/jalik/jalik-ufs/blob/master/ufs-server.js#L310
-const readFromGridFS = function (storeName, fileId, file, req, res) {
+const readFromGridFS = function (
+	storeName: string | undefined,
+	fileId: string,
+	file: IUpload,
+	req: http.IncomingMessage,
+	res: http.ServerResponse,
+) {
+	if (!storeName) {
+		return;
+	}
 	const store = UploadFS.getStore(storeName);
 	const rs = store.getReadStream(fileId, file);
 	const ws = new stream.PassThrough();
@@ -83,7 +98,7 @@ const readFromGridFS = function (storeName, fileId, file, req, res) {
 		return;
 	}
 
-	const accept = req.headers['accept-encoding'] || '';
+	const accept = (Array.isArray(req.headers['accept-encoding']) ? req.headers['accept-encoding'][0] : req.headers['accept-encoding']) || '';
 
 	// Compress data using gzip
 	if (accept.match(/\bgzip\b/)) {
@@ -107,7 +122,11 @@ const readFromGridFS = function (storeName, fileId, file, req, res) {
 	ws.pipe(res);
 };
 
-const copyFromGridFS = function (storeName, fileId, file, out) {
+const copyFromGridFS = function (storeName: string | undefined, fileId: string, file: IUpload, out: stream.Writable) {
+	if (!storeName) {
+		return;
+	}
+
 	const store = UploadFS.getStore(storeName);
 	const rs = store.getReadStream(fileId, file);
 
@@ -139,18 +158,18 @@ FileUpload.configureUploadsStore('GridFS', 'GridFS:Avatars', {
 new FileUploadClass({
 	name: 'GridFS:Uploads',
 
-	get(file, req, res) {
+	async get(file, req, res) {
 		file = FileUpload.addExtensionTo(file);
 
-		res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name)}`);
-		res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
+		res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name || '')}`);
+		file.uploadedAt && res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
 		res.setHeader('Content-Type', file.type || 'application/octet-stream');
-		res.setHeader('Content-Length', file.size);
+		res.setHeader('Content-Length', file.size || 0);
 
-		return readFromGridFS(file.store, file._id, file, req, res);
+		readFromGridFS(file.store, file._id, file, req, res);
 	},
 
-	copy(file, out) {
+	async copy(file, out) {
 		copyFromGridFS(file.store, file._id, file, out);
 	},
 });
@@ -158,18 +177,18 @@ new FileUploadClass({
 new FileUploadClass({
 	name: 'GridFS:UserDataFiles',
 
-	get(file, req, res) {
+	async get(file, req, res) {
 		file = FileUpload.addExtensionTo(file);
 
-		res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name)}`);
-		res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
-		res.setHeader('Content-Type', file.type);
-		res.setHeader('Content-Length', file.size);
+		res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(file.name || '')}`);
+		file.uploadedAt && res.setHeader('Last-Modified', file.uploadedAt.toUTCString());
+		res.setHeader('Content-Type', file.type || '');
+		res.setHeader('Content-Length', file.size || 0);
 
-		return readFromGridFS(file.store, file._id, file, req, res);
+		readFromGridFS(file.store, file._id, file, req, res);
 	},
 
-	copy(file, out) {
+	async copy(file, out) {
 		copyFromGridFS(file.store, file._id, file, out);
 	},
 });
@@ -177,13 +196,13 @@ new FileUploadClass({
 new FileUploadClass({
 	name: 'GridFS:Avatars',
 
-	get(file, req, res) {
+	async get(file, req, res) {
 		file = FileUpload.addExtensionTo(file);
 
-		return readFromGridFS(file.store, file._id, file, req, res);
+		readFromGridFS(file.store, file._id, file, req, res);
 	},
 
-	copy(file, out) {
+	async copy(file, out) {
 		copyFromGridFS(file.store, file._id, file, out);
 	},
 });

commit 6dc7a25c90a31dda15bb5d210308cfd9530f3583

PATCH: AR-3095 Handle failed Image Attachment pipes
The problem is two-fold:

  1. S3 authentication sometimes fails because we no longer
	   pass a callback to `getSignedUrl()`.  Were we to pass a
		 callback, the expired, temporary credentials would be refreshed.
		 This was broken by commit 90238b9c1538a1d64268926a1861def4ff1bfcc9
		 in apps/meteor/app/file-upload/ufs/AmazonS3/server.ts

  2. When proxying the file from S3, if the authentication fails,
	   or there is any other error leading to us not receiving the
		 file, we blindly pass along the (probably empty) body as if
		 that is the actual file.

Solutions:

  1. If the authentication fails, return 500.
	   Fastly will retry and presumably succeed the second time
		 around.  Or the user can click "Retry" and likewise succeed.
	2. If any other error occurs, return 500.
	   Same consequences as above.

Solution 2 is more catch-all.
Solution 1 saves us from having to hit S3 needlessly.

apps/meteor/app/file-upload/server/config/AmazonS3.ts
@@ -14,7 +14,14 @@ const get: FileUploadClass['get'] = async function (this: FileUploadClass, file,
 	const forceDownload = typeof query.download !== 'undefined';
 
 	const fileUrl = await this.store.getRedirectURL(file, forceDownload);
-	if (!fileUrl || !file.store) {
+	if (!fileUrl || !file.store || fileUrl === 'https://s3.us-west-2.amazonaws.com/') {
+		// If the credentials fail, getRedirectURL can return the string above,
+		// which just 307s (incidentally to https://aws.amazon.com/s3/).
+		// Proxying that 307 (in proxyFile(), below) just sends an empty body and closes the connection,
+		// causing us to falsey return an empty file.
+		// HACK: just fail 500 and hopefully it will succeed the next time around.
+		res.setHeader('x-rc-debug-s3url', fileUrl || '');
+		res.writeHead(500);
 		res.end();
 		return;
 	}

apps/meteor/app/file-upload/server/lib/FileUpload.ts
@@ -562,7 +562,21 @@ export const FileUpload = {
 	) {
 		res.setHeader('Content-Disposition', `${forceDownload ? 'attachment' : 'inline'}; filename="${encodeURI(fileName)}"`);
 
-		request.get(fileUrl, (fileRes) => fileRes.pipe(res));
+		request.get(fileUrl, (fileRes) => {
+			if (fileRes.statusCode !== 200) {
+				res.setHeader('x-rc-proxyfile-status', fileRes.statusCode);
+				res.setHeader('content-length', 0);
+				res.writeHead(500);
+				res.end();
+				return;
+			}
+
+			['content-length', 'content-type'].forEach((header) => {
+				fileRes.headers[header] && res.setHeader(header, fileRes.headers[header]);
+			});
+
+			fileRes.pipe(res);
+		});
 	},
 
 	generateJWTToFileUrls({ rid, userId, fileId }: { rid: string; userId: string; fileId: string }) {