dcfe91b731e6087326771956ea13129261e75fe5
Noach Magedman <noach@seekingalpha.com>
2023-09-28 12:02:29 +0300

    PATCH: Log `/file-upload/` requests

--- apps/meteor/app/file-upload/server/lib/requests.ts
+++ apps/meteor/app/file-upload/server/lib/requests.ts
@@ -2,8 +2,22 @@ import { Uploads } from '@rocket.chat/models';
 import { WebApp } from 'meteor/webapp';
 
 import { FileUpload } from './FileUpload';
+import { Logger } from '@rocket.chat/logger';
 
 WebApp.connectHandlers.use(FileUpload.getPath(), async (req, res, next) => {
+	const log = new Logger('FileUpload').logger.child({
+		url: req.url,
+		userAgent: req.headers['user-agent'],
+		userId: req.headers['x-user-id'],
+		host: req.headers.host,
+		referer: req.headers.referer,
+		fastly: req.headers['fastly-client'],
+	});
+
+	log.http({
+		stage: 'start',
+	});
+
 	const match = /^\/([^\/]+)\/(.*)/.exec(req.url || '');
 
 	if (match?.[1]) {
