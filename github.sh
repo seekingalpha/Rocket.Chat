eff659718958d7b9548c0230df2c967c9a41a617
Noach Magedman <noach@seekingalpha.com>
2024-04-08 15:17:06 +0300

    Re-organize github.sh
    - Gather all needed data before beginning the deployment.
      e.g. Get our Fastly credentials from SSM before deploying
      the new build to our RC EC2 instances, lest we find belatedly
      that we lack permissions and are left in a broken state.
    - Flush the Fastly CDN **ASAP** after deploying the new build,
      to minimize any downtime.  We can record the S3 marker file
      afterward.

--- github.sh
+++ github.sh
@@ -10,6 +10,11 @@ function hr() {
   echo "==========================================================================="
 }
 
+hr
+
+
+## Gather all needed data
+
 rc_dir=/opt/rocket-chat
 s3_bucket=seekingalpha-rocketchat-builds
 
@@ -21,10 +26,30 @@ else
   rc_tarball="rocket.chat-$version.tgz"
 fi
 
-## Strip off the trailing letter from the region: Use us-west-2, not us-west-2a
+# Strip off the trailing letter from the region: Use us-west-2, not us-west-2a
 export AWS_DEFAULT_REGION=$(ec2metadata --availability-zone | awk '{print substr($0,1,length($0)-1)}')
 
-## EXPORTED variables ending in _ENVSUBST are for expansion in the .tpl template files.
+# We will need to flush the Fastly cache post-deployment because it caches 404s returned by an _old_ server
+# which was asked for a versionized file referenced by a _new_ server.
+# (Now that we are HUPing servers in parallel rather than serially, the window for this race condition is much smaller,
+# however it might not be completely eliminated.)
+fastly_service=$(aws ssm get-parameter --name /rocketchat/fastly_service_id --with-decryption --query Parameter.Value --output text)
+fastly_token=$(aws ssm get-parameter --name /rocketchat/fastly_api_key --with-decryption --query Parameter.Value --output text)
+
+# Get RC EC2 instance IPs (one per line, as a multiline string, since `parallel-ssh --hosts` expects them in that format)
+rc_instance_ips=$(
+  aws ec2 describe-instances \
+      --filters Name=instance-state-name,Values=running \
+                Name=tag:aws:autoscaling:groupName,Values=rocketchat \
+      --query "Reservations[*].Instances[*].NetworkInterfaces[0].PrivateIpAddress" \
+      --output text
+)
+
+
+## Generate scripts to run on the RC EC2 instances, using the `envsubst` template renderer,
+## part of the GNU gettext package.
+
+# *Exported* variables ending in _ENVSUBST will be expanded in the *.tpl template files.
 export AWS_DEFAULT_REGION_ENVSUBST=$AWS_DEFAULT_REGION
 export ENV_ENVSUBST=$environment
 export RC_DIR_ENVSUBST=$rc_dir
@@ -34,47 +59,36 @@ export S3_BUCKET_ENVSUBST=$s3_bucket
 #append all "_ENVSUBST" env vars keys to a online commas separated.
 b=""; for i in $(printenv | grep "_ENVSUBST" | sed 's;=.*;;'); do echo "$i"; b="$b\$$i,"; done; b=${b::-1};
 envsubst_varlist="$b"
+envsubst "$envsubst_varlist" < pre_install.sh.tpl    > pre_install.sh
+envsubst "$envsubst_varlist" < rotate_version.sh.tpl > rotate_version.sh
 
-## Render Script Templates
-envsubst "$envsubst_varlist" < ./pre_install.sh.tpl  > ./pre_install.sh
-envsubst "$envsubst_varlist" < ./rotate_version.sh.tpl > ./rotate_version.sh
-
-## Get instance IPs one per line (multiline string)
-rc_instance_ips=$(
-  aws ec2 describe-instances \
-      --filters Name=instance-state-name,Values=running \
-                Name=tag:aws:autoscaling:groupName,Values=rocketchat \
-      --query "Reservations[*].Instances[*].NetworkInterfaces[0].PrivateIpAddress" \
-      --output text
-)
 
 ## Install RC tarball (and its dependencies) onto all RC nodes
-hr
-echo "Installing new build onto all RC nodes:"
+echo "Installing new build onto all RC nodes..."
 parallel-ssh \
   -x  "-o StrictHostKeyChecking=no" \
   --inline --timeout 600 --user deploy \
   --hosts <(echo "$rc_instance_ips") \
-  --send-input < ./pre_install.sh
+  --send-input < pre_install.sh            # TODO: Rename to install_tarball.sh
 hr
 
 ## Activate new version
-echo "Activating new build on all RC nodes:"
+echo "Activating new build on all RC nodes..."
 parallel-ssh \
   -x  "-o StrictHostKeyChecking=no" \
   --inline --timeout 600 --user deploy \
   --hosts <(echo "$rc_instance_ips") \
-  --send-input < ./rotate_version.sh
-hr
-
-## Update the version marker file
-echo "Mark which RC build is now active..."
-current_marker_file="rocket.chat-$environment.tgz"
-aws s3 cp "s3://$s3_bucket/$rc_tarball" "s3://$s3_bucket/$current_marker_file" --acl public-read
+  --send-input < rotate_version.sh         # TODO: Rename to activate_new_build.sh
 hr
 
 ## Flush CDN
-echo "Flushing $environment CDN"
-FASTLY_SERVICE=$(aws ssm get-parameter --name /rocketchat/fastly_service_id --with-decryption --query Parameter.Value --output text)
-FASTLY_TOKEN=$(aws ssm get-parameter --name /rocketchat/fastly_api_key --with-decryption --query Parameter.Value --output text)
-curl -X POST -H "Fastly-Key: $FASTLY_TOKEN" "https://api.fastly.com/service/$FASTLY_SERVICE/purge/$environment"
+echo "Flushing $environment CDN..."
+curl -X POST -H "Fastly-Key: $fastly_token" "https://api.fastly.com/service/$fastly_service/purge/$environment"
+hr
+
+## Update the version marker file
+echo "Mark (in S3) which RC build is now active on $environment..."
+aws s3 cp "s3://$s3_bucket/$rc_tarball" "s3://$s3_bucket/rocket.chat-$environment.tgz" --acl public-read
+hr
+
+echo Done!
