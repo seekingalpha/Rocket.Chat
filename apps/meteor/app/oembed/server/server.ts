50b455db69fab84e4b91ce2ded76518be26e7ac1
Noach Magedman <noach@seekingalpha.com>
2024-06-03 16:10:19 +0300

    PATCH: Add "SpeakEasy" HTTP header when loading Link Preview from SAPI
    This header name/value is recognized by PerimeterX, whitelisting us.
    It can also be added to SAPI if we need whitelisting there.

--- apps/meteor/app/oembed/server/server.ts
+++ apps/meteor/app/oembed/server/server.ts
@@ -99,6 +99,11 @@ const getUrlContent = async (urlObj: URL, redirectCount = 5): Promise<OEmbedUrlC
 	const sizeLimit = 250000;
 
 	log.debug(`Fetching ${url} following redirects ${redirectCount} times`);
+
+	const hittingSAPI =
+		(process.env.SPEAKEASY_HTTP_HEADER_NAME && process.env.SPEAKEASY_HTTP_HEADER_VALUE) &&
+		(urlObj.hostname === 'seekingalpha.com' || urlObj.hostname.endsWith('.seekingalpha.com'));
+
 	const response = await fetch(
 		url,
 		{
@@ -108,6 +113,7 @@ const getUrlContent = async (urlObj: URL, redirectCount = 5): Promise<OEmbedUrlC
 				'User-Agent': `${settings.get('API_Embed_UserAgent')} Rocket.Chat/${Info.version}`,
 				'Accept-Language': settings.get('Language') || 'en',
 				...data.headerOverrides,
+				...(hittingSAPI ? { [process.env.SPEAKEASY_HTTP_HEADER_NAME]: process.env.SPEAKEASY_HTTP_HEADER_VALUE } : {}),
 			},
 			size: sizeLimit, // max size of the response body, this was not working as expected so I'm also manually verifying that on the iterator
 		},
