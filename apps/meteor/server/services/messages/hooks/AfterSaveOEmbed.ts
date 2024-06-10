1a765b87e7de0ae3fd14d25e537969eba50b3090
Noach Magedman <noach@seekingalpha.com>
2024-06-10 16:21:00 +0300

    PATCH: Increase the OEmbed sizeLimit from 250 KB to 1 MB
    SAPI IG Articles have massive `<head>` elements, with massive `<style>`
    blocks, which push the `<meta>` tags we are interested in beyond
    vanilla’s 250 KB `fetch()` sizeLimit, causing Preview Links to miss out
    on important data.
    
    A copy of the fetched html is stored in
    `db.rocketchat_oembed_cache#data.content.body`, so increasing
    the sizeLimit impacts our storage requirements.  In practice,
    the impact isn't so great.  Pages are cached only for a week.
    On Mongo/Production, we have under 1500 docs, only 600 of which
    reach the 250k limit.  Assuming _all_ of those would hit the higher
    1 MB limit, that's an increased storage of 450 MB.
    
    ```
    ((1 * 1024 * 1024) - (250 * 1024)) * 600 / 1024 / 1024  //=> 453.5 MB
    ```
    
    TODO:
    An improvement on this would be to extract out only the `<head>`
    element from the html, strip out `<style>` and `<script>` elements
    from it, and store the rest.

--- apps/meteor/server/services/messages/hooks/AfterSaveOEmbed.ts
+++ apps/meteor/server/services/messages/hooks/AfterSaveOEmbed.ts
@@ -133,7 +133,7 @@ const getUrlContent = async (urlObj: URL, redirectCount = 5): Promise<OEmbedUrlC
 	const data = beforeGetUrlContent({ urlObj });
 
 	const url = data.urlObj.toString();
-	const sizeLimit = 250000;
+	const sizeLimit = 1 * 1024 * 1024;  // 1 MB. Was originally 250 KB, but SAPI MP articles exceed that 2-fold.  // 250000;
 
 	log.debug({ msg: 'Fetching URL for OEmbed', url, redirectCount });
 	const start = Date.now();
