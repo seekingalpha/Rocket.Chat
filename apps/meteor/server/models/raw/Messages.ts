f2f415c2957a99f193c49c37b2dc9ba583f171ae
Noach Magedman <noach@seekingalpha.com>
2024-09-17 15:40:34 +0300

    PATCH: [TMP] AR-3427 Limit `chat.syncMessages` to syncing recent messages only
    Only sync updated messages which were created within the last 30 days.
    Older messages will still show old data (usernames, text edits, etc.)
    
    This endpoint is only used by the mobile app, which maintains its local copy
    of the Message collection.  Only already-loaded messages need be updated by
    this mechanism.  Newly fetched history is fetched by other means.

--- apps/meteor/server/models/raw/Messages.ts
+++ apps/meteor/server/models/raw/Messages.ts
@@ -880,13 +880,21 @@ export class MessagesRaw extends BaseRaw<IMessage> implements IMessagesModel {
 	}
 
 	findForUpdates(roomId: string, timestamp: Date, options?: FindOptions<IMessage>): FindCursor<IMessage> {
+		// HACK: To support bulk username renames without causing massive load (due to fetching every ancient post),
+		// only fetch **recently created** messages.
+		const duration_30_days_in_ms = 30 * 24 * 60 * 60 * 1000;
+		const date_30_days_ago = new Date(Date.now() - duration_30_days_in_ms);
+
 		const query = {
-			_hidden: {
-				$ne: true,
-			},
 			rid: roomId,
+			ts: {
+				$gte: date_30_days_ago,
+			},
 			_updatedAt: {
 				$gt: timestamp,
+			},
+			_hidden: {
+				$ne: true,
 			},
 		};
 		return this.find(query, options);
